e4abf09e20cf5f3819e093f91be705660aaff413
saejim <saejim91@gmail.com>
2022-06-22 07:00:10 +0600

    updated HelperFunction

--- src/Components/HelperFunc.js
+++ src/Components/HelperFunc.js
@@ -1,3 +1,15 @@
+
+//DISPLAY
+export default function setDisplay(el, on = false) {
+	if (on === true) {
+		el.style.display = "flex";
+	} else {
+		el.style.display = "none";
+	}
+}
+
+
+//FOR CANVAS
 export function clearCanvas(ctx, canva) {
 	ctx.clearRect(0, 0, canva.width, canva.height);
 }
@@ -11,3 +23,23 @@ export function writeOnCanvasWithFont(ctx, userText, font) {
 	ctx.fillStyle = 'White';
 	ctx.fillText(userText, 0, 50);
 }
+
+
+//MEASUREMENT
+
+
+
+
+
+
+
+// export function debounceMeasurement() {
+// 	let timeout;
+// 	showBars(false);
+// 	//cleartimeout
+// 	clearTimeout(timeout);
+// 	//measure bar
+// 	timeout = setTimeout(() => {
+// 		measureBars(metrics, textLength, barWidthSize, barHeight, barHeightSize);
+// 	}, 3000);
+// }
