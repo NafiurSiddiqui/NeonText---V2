8ff56ce5e541cc97d1f45062e828bb3570daf237
saejim <saejim91@gmail.com>
2022-06-23 07:59:41 +0600

    $1

--- src/Components/UI/Ui-input/input/UiText.js
+++ src/Components/UI/Ui-input/input/UiText.js
@@ -22,32 +22,6 @@ function UiText(props) {
 
 	const userTextChangeHandler = (e) => {
 		setUserText(e.target.value);
-
-		//any space should be omitted from calculating
-		if (e.nativeEvent.data === ' ') {
-			return;
-		}
-		//Displaying text and calculation
-		if (e.nativeEvent.inputType === 'deleteContentBackward') {
-			//recapture the userText here
-
-			let delUserText = setUserText(e.target.value);
-
-			props.onDelUserText(delUserText);
-
-			//RERENDER THE USERTEXT
-			if (delUserText.length !== 0) {
-				// clearCanvas(ctx, canva);
-				// writeOnCanvas(ctx, delUserText);
-				// metrics = ctx.measureText(userText);
-				// debounceMeasurement();
-			}
-		} else {
-			//SIMPLY RENDER THE TEXT
-			// writeOnCanvas(ctx, userText);
-			// metrics = ctx.measureText(userText);
-			// debounceMeasurement();
-		}
 	};
 
 	// console.log(userText);
--- src/Components/UI/Ui.js
+++ src/Components/UI/Ui.js
@@ -1,4 +1,5 @@
-import UserTextContext from '../../store/text-context';
+import { useState } from 'react';
+
 import Header from '../Header';
 import UiDisplay from './Ui-display/UiDisplay';
 import UserInput from './Ui-input/input/UserInput';
@@ -7,13 +8,36 @@ function Ui() {
 	//HERE looks like a good place to pass around data
 	//newUserText?
 	//userText?
+	const [userText, setNewUserText] = useState('');
+
+	const calculationHandler = (e) => {
+		//any space should be omitted from calculating
+		if (e.nativeEvent.data === ' ') {
+			return;
+		}
+		//Displaying text and calculation
+		if (e.nativeEvent.inputType === 'deleteContentBackward') {
+			//recapture the userText here
+
+			let delUserText = setNewUserText(e.target.value);
+
+			//RERENDER THE USERTEXT
+			if (delUserText.length !== 0) {
+				// clearCanvas(ctx, canva);
+				// writeOnCanvas(ctx, delUserText);
+				// metrics = ctx.measureText(userText);
+				// debounceMeasurement();
+			}
+		} else {
+			//SIMPLY RENDER THE TEXT
+			// writeOnCanvas(ctx, userText);
+			// metrics = ctx.measureText(userText);
+			// debounceMeasurement();
+		}
+	};
+
 	return (
-		<UserTextContext.Provider
-			value={{
-				userText: '',
-				newUserText: null,
-			}}
-		>
+		<>
 			<Header className="ui-logo" />
 			<main className="container">
 				<section className="ui-container">
@@ -22,7 +46,7 @@ function Ui() {
 					<UiDisplay />
 				</section>
 			</main>
-		</UserTextContext.Provider>
+		</>
 	);
 }
 export default Ui;
