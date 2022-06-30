df805e26c5110466087334c672d835c214776360
saejim <saejim91@gmail.com>
2022-07-01 02:41:21 +0600

    FontBtn functionality successfully added

---
+++ src/Components/UI/Ui-input/input/BtnFont.js
@@ -0,0 +1,18 @@
+// import ImagesBtn from '../../../Images';
+
+function BtnFont(props) {
+	return (
+		<>
+			<li className={props.className} onClick={props.onClick} key={props.keys}>
+				<img
+					className="ui-input-fontFamily-list__image"
+					src={props.image}
+					alt="Font family"
+					id={props.id}
+					onClick={props.onClick}
+				/>
+			</li>
+		</>
+	);
+}
+export default BtnFont;
--- src/Components/UI/Ui-input/input/UiFonts.js
+++ src/Components/UI/Ui-input/input/UiFonts.js
@@ -1,39 +1,97 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import ImagesBtn from '../../../Images';
+// import ImagesBtn from '../../../Images';
+import BtnFont from './BtnFont';
+
+
+
+const buttons = [
+	{
+		liClass: 'Amanda',
+		id: 'Amanda',
+		image: ImagesBtn.amanda,
+		key: 1,
+	},
+	{
+		liClass: 'Allura',
+		id: 'Allura',
+		image: ImagesBtn.allura,
+		key: 2,
+	},
+	{
+		liClass: 'Tangerine',
+		id: 'Tangerine',
+		image: ImagesBtn.tangerine,
+		key: 3,
+	},
+	{
+		liClass: 'Amsterdam',
+		id: 'Amsterdam',
+		image: ImagesBtn.amsterdam,
+		key: 4,
+	},
+	{
+		liClass: 'Raster',
+		id: 'Raster',
+		image: ImagesBtn.raster,
+		key: 5,
+	},
+	{
+		liClass: 'HerrVonMuellerhoff',
+		id: 'HerrVonMuellerhoff',
+		image: ImagesBtn.herrVonMuellerhoff,
+		key: 6,
+	},
+	{
+		liClass: 'Cookie',
+		id: 'Cookie',
+		image: ImagesBtn.cookie,
+		key: 7,
+	},
+	{
+		liClass: 'Orbitron',
+		id: 'Orbitron',
+		image: ImagesBtn.orbitron,
+		key: 8,
+	},
+];
+
+
+
+
 
 function UiFonts(props) {
-	const [btnActive, setbtnActive] = useState();
+	// const [btnActive, setbtnActive] = useState();
+	const [activeButtonId, setActiveButtonId] = useState(null);
 
-	const btnValue = useRef();
+	
 
 	const fontHandler = (e) => {
+
 		//if--click is list
 		if (e.target.localName === 'li') {
 			//get the fontName
 			const targetClass = e.target.classList[1];
 			props.getFontFamily(targetClass);
 			props.getFontState(true);
-			// setbtnActive(true);
-
-			console.log(targetClass);
-
-			//if - current Id matches with the listOfFonts
-
-			// console.log(what[0].btnActive);
-
-			//-----set that  status to true
-			//---- that status is True && add btnActive to that id
+			setActiveButtonId(targetClass);
+		
+		
 		}
-
+		//if--img
 		if (e.target.localName === 'img') {
-			props.getFontFamily(e.target.id);
+			const targetId = e.target.id
+			props.getFontFamily(targetId);
 			props.getFontState(true);
-			setbtnActive(true);
+			setActiveButtonId(targetId);
+		
 		}
 	};
-	// console.log(btnValue);
-	// const btnActivation = btnActive ? 'btn-active' : '';
 
+	
+
+	
+	
 	return (
 		<section
 			className={`ui-input-form fontFamily  ${
@@ -42,94 +100,25 @@ function UiFonts(props) {
 		>
 			<h3 className="ui-input-form-heading">CHOOSE FONT</h3>
 			<ul className="ui-input-fontFamily-lists">
-				<li
-					className={`ui-input-fontFamily-list Amanda  `}
-					ref={btnValue}
-					onClick={fontHandler}
-				>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.amanda}
-						alt="Font family Amanda"
-						id="Amanda"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li className={`ui-input-fontFamily-list Allura`} onClick={fontHandler}>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.allura}
-						alt="Font family Allura"
-						id="Allura"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li
-					className="ui-input-fontFamily-list Tangerine"
-					onClick={fontHandler}
-				>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.tangerine}
-						alt="Font Family Tangerine"
-						id="Tangerine"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li
-					className="ui-input-fontFamily-list Amsterdam"
-					onClick={fontHandler}
-				>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.amsterdam}
-						alt="Font Family"
-						id="Amsterdam"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li
-					className="ui-input-fontFamily-list RasterSlice"
-					onClick={fontHandler}
-				>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.raster}
-						alt="Font family Raster"
-						id="RasterSlice"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li
-					className="ui-input-fontFamily-list HerrVonMuellerhoff"
-					onClick={fontHandler}
-				>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.herrVonMuellerhoff}
-						alt="Font family Herr von Muellerhoff"
-						id="HerrVonMuellerhoff"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li className="ui-input-fontFamily-list Cookie" onClick={fontHandler}>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.cookie}
-						alt="Font family Cookie"
-						id="Cookie"
-						onClick={fontHandler}
-					/>
-				</li>
-				<li className="ui-input-fontFamily-list Orbitron" onClick={fontHandler}>
-					<img
-						className="ui-input-fontFamily-list__image"
-						src={ImagesBtn.orbitron}
-						alt="Font family Orbitron"
-						id="Orbitron"
-						onClick={fontHandler}
-					/>
-				</li>
+				{buttons.map((btn) => {
+					const isActive = btn.id === activeButtonId;
+					// console.log(`Button ID: ${btn.id}`);
+					// console.log(`ActiveBtn Id: ${activeButtonId}`);
+					return (
+						<BtnFont
+							className={`ui-input-fontFamily-list ${btn.liClass} ${
+								isActive ? 'btn-active' : ''
+							}`}
+							// onClick={() => {
+							// 	setActiveButtonId(btn.id);
+							// }}
+							onClick={fontHandler}
+							key={btn.key}
+							id={btn.id}
+							image={btn.image}
+						/>
+					);
+				})}
 			</ul>
 		</section>
 	);
