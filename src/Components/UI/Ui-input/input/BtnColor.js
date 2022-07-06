44bae111905b5f2bd1bb449bf0929c32de3f46df
saejim <saejim91@gmail.com>
2022-07-07 02:06:17 +0600

    uiColor: hover code refactored

--- src/Components/UI/Ui-input/input/BtnColor.js
+++ src/Components/UI/Ui-input/input/BtnColor.js
@@ -4,11 +4,13 @@ function BtnColor(props) {
 	let glow = `0 0 4px white, 0 0 4px ${props.targetColor}, 0 0 8px ${props.targetColor},
 	0 0 12px ${props.targetColor}, 0 0 16px ${props.targetColor}, 0 0 18px ${props.targetColor}`;
 
+	const hoverGlow = `${props.hoverActive && props.targetBulb? glow:'none'}`
+
 	return (
 		<li className={`input-color-list ${props.colorClass}`} title="Some cool Description of this color" onClick={props.onClick} key={props.keys} onMouseEnter={props.onMouseOver} onMouseLeave={props.onMouseOut} data-colorcode={props.colorCode} ref={props.refs}>
 
 			<i className="fa fa-lightbulb-o"  data-active={props.dataActive} onClick={props.onClick} data-color={props.colorClass}
-			style={{textShadow: `${props.hoverActive && props.targetBulb? glow:'none'}`}}
+			style={{textShadow:hoverGlow }}
 			></i>
 			<p className="input-color-list-heading">{props.colorName}</p>
 		</li>
